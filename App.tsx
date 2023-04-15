875c7ecd68ef1790d29d73941c8f7fb48967b7a0
yousefed <yousefdardiry@gmail.com>
2023-04-15 14:21:12 +0200

    wip

--- App.tsx
+++ App.tsx
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { BarCodeEvent, BarCodeScanner } from "expo-barcode-scanner";
+import { Camera, CameraType } from "expo-camera";
+import React, { useRef, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 export default function App() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const ref = useRef<Camera>();
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function capture() {
+    ref.current?.takePictureAsync();
+    // setType((current) =>
+    //   current === CameraType.back ? CameraType.front : CameraType.back
+    // );
+  }
+
+  const _handleBarCodeScanned = (params: BarCodeEvent) => {};
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <BarCodeScanner
+        onBarCodeScanned={_handleBarCodeScanned}
+        style={{
+          height: "50%",
+          width: "50%",
+        }}
+      />
+      {/* <Camera ref={ref} style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={capture}>
+            <Text style={styles.text}>Capture my drawing!</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera> */}
     </View>
   );
 }
@@ -13,8 +56,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
   },
 });
